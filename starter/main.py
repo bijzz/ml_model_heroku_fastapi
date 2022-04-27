723a31a366d5d7b230b9afc7a6701ecf7d77a972
bijzz <user@anonynmous.com>
2022-04-27 22:36:57 +0200

    add aptfile - dvc logic for heroku

--- starter/main.py
+++ starter/main.py
@@ -8,6 +8,14 @@ import pandas as pd
 # Instantiate the app.
 app = FastAPI()
 
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 
 @app.get("/")
 async def welcome():
